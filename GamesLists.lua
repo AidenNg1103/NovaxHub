cdd2231993e4bd43aedd740d9a4c56928a796773
AidenNg1103 <136418200+AidenNg1103@users.noreply.github.com>
2024-05-30 06:41:39 -0700

    GamesLists.lua

---
+++ GamesLists.lua
@@ -0,0 +1,7 @@
+local Games = {
+    [111958650] = "https://raw.githubusercontent.com/AidenNg1103/NovaxHub/main/Arsenal.lua",
+    [1119466531] = "https://raw.githubusercontent.com/AidenNg1103/NovaxHub/main/LegendOfSpeed.lua",
+    [1335695570] = "https://raw.githubusercontent.com/AidenNg1103/NovaxHub/main/NinjaLegend.lua",
+}
+
+return Games
